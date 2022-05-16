da3232f2f4a73605b757b8e52180467911830b5c
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-05-16 13:44:47 +0200

    feat: implement basic receipt

---
+++ cashier/cash_register/register.py
@@ -0,0 +1,24 @@
+import datetime
+from collections import namedtuple
+
+PriceInfo = namedtuple('PriceInfo', 'price tax')
+
+class Receipt:
+    def __init__(self):
+        self.time = datetime.datetime.now()
+        self.items = dict()
+        self.total_sales_tax = 0
+        self.total_price = 0
+
+    def add_item(self, item, sales_tax_rate):
+        price = item.price * item.quantity
+        sales_tax = price * sales_tax_rate
+        taxed_price = price + sales_tax
+        # TODO: handle when item already exists
+        self.items[item] = PriceInfo(taxed_price, sales_tax)
+    
+    def remove_item(self, item):
+        del self.items[item]
+
+    def get_items(self):
+        return self.items
