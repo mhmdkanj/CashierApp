759d8bf180d382ba69fbdf1d8c0273b78072fe16
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-05-16 21:10:41 +0200

    fix: string input for replace method in ReceiptPrinter

--- cashier/cash_register/utils.py
+++ cashier/cash_register/utils.py
@@ -17,15 +17,15 @@ class ReceiptPrinter:
 
     @staticmethod
     def generate_raw_receipt(receipt):
-        raw_receipt = ReceiptPrinter.template.replace("$DATETIME", receipt.time)
+        raw_receipt = ReceiptPrinter.template.replace("$DATETIME", str(receipt.time))
         
         item_log = ''
         for item, (taxed_price, _) in receipt.get_items().items():
             item_log += f"{item} --- {taxed_price}\n"
         raw_receipt = raw_receipt.replace("$ITEMS", item_log)
 
-        raw_receipt = raw_receipt.replace("$TOTAL_SALES_TAX", receipt.total_sales_tax)
-        raw_receipt = raw_receipt.replace("$TOTAL_PRICE", receipt.total_price)
+        raw_receipt = raw_receipt.replace("$TOTAL_SALES_TAX", str(receipt.total_sales_tax))
+        raw_receipt = raw_receipt.replace("$TOTAL_PRICE", str(receipt.total_price))
         return raw_receipt
 
     @staticmethod
