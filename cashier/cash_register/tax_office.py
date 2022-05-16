2d2ba78ace86dc919c2585ed4955017d49e7af34
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-05-16 12:55:40 +0200

    feat: implement tax office

--- cashier/cash_register/tax_office.py
+++ cashier/cash_register/tax_office.py
@@ -6,3 +6,14 @@ class ItemCategory(Enum):
     FOOD = 1
     BOOK = 2
     MEDICAL = 3
+
+
+class TaxOffice:
+    @staticmethod
+    def calculate_tax_rate(category, imported):
+        sales_tax_rate = 0
+        if imported:
+            sales_tax_rate += 0.05
+        if category == ItemCategory.NON_ESSENTIAL:
+            sales_tax_rate += 0.1
+        return sales_tax_rate
